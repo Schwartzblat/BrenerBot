b845b3293fe779d4c1ad489126f97c7e42bd684a
Martin Alebachew <martinalebachew@gmail.com>
2023-04-08 21:55:53 +0300

    Temporary disabled non-supported commands
    - Temporary disabled non-supported commands. Support for those commands on the websocket version will be added on future commits.

--- src/commands/admin/shutdown.ts
+++ src/commands/admin/shutdown.ts
@@ -1,26 +1,26 @@
-// shutdown.ts
-// (C) Martin Alebachew, 2023
-
-import { Command, GroupChatPermissions, PrivateChatPermissions } from "../commands"
-import { cleanShutdown } from "../../index"
-import { Client, Message } from 'whatsapp-web.js'
-
-const NATIVE_SHUTDOWN_MESSAGE = "להתראות 👋"
-
-let command: Command = {
-    permissions: {
-        groupChat: GroupChatPermissions.Owner,
-        privateChat: PrivateChatPermissions.Owner
-    },
-    nativeText: {
-        name: "כיבוי",
-        description: "מי אני ומה שמי"
-    },
-    async execute(client: Client, msg: Message, args: string[]) {
-        if (args.length) return
-        await msg.reply(NATIVE_SHUTDOWN_MESSAGE)
-        setTimeout(cleanShutdown, 3000)  // Wait for message to flush
-    }
-}
-
-module.exports = command
+// // shutdown.ts
+// // (C) Martin Alebachew, 2023
+//
+// import { Command, GroupChatPermissions, PrivateChatPermissions } from "../commands"
+// import { cleanShutdown } from "../../index"
+// import { Client, Message } from 'whatsapp-web.js'
+//
+// const NATIVE_SHUTDOWN_MESSAGE = "להתראות 👋"
+//
+// let command: Command = {
+//     permissions: {
+//         groupChat: GroupChatPermissions.Owner,
+//         privateChat: PrivateChatPermissions.Owner
+//     },
+//     nativeText: {
+//         name: "כיבוי",
+//         description: "מי אני ומה שמי"
+//     },
+//     async execute(client: Client, msg: Message, args: string[]) {
+//         if (args.length) return
+//         await msg.reply(NATIVE_SHUTDOWN_MESSAGE)
+//         setTimeout(cleanShutdown, 3000)  // Wait for message to flush
+//     }
+// }
+//
+// module.exports = command
