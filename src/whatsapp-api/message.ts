7a1e27b22149dc47fa927247c784fa97a6a9ce04
Martin Alebachew <martinalebachew@gmail.com>
2023-04-08 18:18:04 +0300

    Implemented constructors
    - Implemented MessageBase and TextMessage as classes with a constructor.

--- src/whatsapp-api/message.ts
+++ src/whatsapp-api/message.ts
@@ -3,17 +3,30 @@
 
 import { GroupAddress, UserAddress } from "./address"
 
-export interface MessageBase {
-    chatId: UserAddress | GroupAddress
-    author: UserAddress
-    type: string
+export class MessageBase {
+    public author: UserAddress
+    public chat: UserAddress | GroupAddress
+
+    constructor(author: UserAddress, chat: UserAddress | GroupAddress) {
+        this.author = author
+        this.chat = chat
+    }
 }
 
-export interface TextMessage extends MessageBase {
-    text: string
+export class TextMessage extends MessageBase {
+    public text: string
+
+    constructor(author: UserAddress, chat: UserAddress | GroupAddress, text: string) {
+        super(author, chat)
+        this.text = text
+    }
 }
 
 // export interface ImageMessage extends MessageBase {
 //     text: string
 //     image:
 // }
+
+export function wrapMessage(message: WAMessage): MessageBase {
+
+}
